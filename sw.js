72ab8b5eb117501d40132fdf59e84682c6cf005b
Calvin B <cbissett88@gmail.com>
2018-08-23 09:21:01 -0400

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,48 @@
+var staticCache = 'restaurant-reviews-cache';
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('restaurant-reviews-cache').then(function(cache) {
+            return cache.addAll([
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+                'index.html',
+                '/restaurant.html',
+                '/data/restaurants.json',
+                '/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return cache.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
+
